67058233fd8dc4fd32a3b4a76bc043cb76e30303
grosariodev <165927811+grosariodev@users.noreply.github.com>
2025-01-03 10:54:33 -0400

    Completed Section 4: Numbers in JS

---
+++ JSmoderno/Curso JS Moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,11 @@
+let puntaje = 5;
+
+//puntaje++; // Esto retorna 5
+//++puntaje; // Esto retorna 6
+
+// En casos donde quieras que el incremento no sea solo de 1 en 1, puedes hacer:
+
+puntaje += 2;
+
+
+console.log(puntaje);
---
+++ JSmoderno/Curso JS Moderno/04-Numeros/js/06-app.js
@@ -0,0 +1,3 @@
+const numero1 = "20"
+console.log(numero1);
+console.log(Number.parseInt(numero1));
