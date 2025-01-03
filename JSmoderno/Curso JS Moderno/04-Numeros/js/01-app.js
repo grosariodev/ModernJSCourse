184166074c55b9145b620928a78381fa30416f0e
grosariodev <165927811+grosariodev@users.noreply.github.com>
2025-01-02 23:51:23 -0400

    Started studying numbers in JS but had to pause due to work commitments

---
+++ JSmoderno/Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,12 @@
+// Como crear numeros en JS
+
+// Este ya no es un numero, es un string que contiene un numero.
+const numero1 = "20";
+
+// Este es la forma comun de hacer un numero.
+const numero2 = 20;
+
+// En JS al no ser tipado no es necesario poner el tipo de numero que se pondra, JS interpreata o entiende a todos los numeros:
+const numero3 = 2.10;
+const numero4 = .20;
+const numero5 = -20;
---
+++ JSmoderno/Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,11 @@
+const numero1 = 30;
+const numero2 = 20;
+
+
+let resultado;
+
+//Suma
+resultado = numero1 + numero2;
+
+
+console.log(resultado);
