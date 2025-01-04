f3f401a38cb6494bfbca6244d8224d46ded3486b
grosariodev <165927811+grosariodev@users.noreply.github.com>
2025-01-04 15:07:15 -0400

    Completed Section 6: Booleans in JS

--- JSmoderno/Curso JS Moderno/05-Operadores/js/02-app.js
+++ JSmoderno/Curso JS Moderno/05-Operadores/js/02-app.js
@@ -1,23 +1,25 @@
-
 const numero1 = 20;
 const numero2 = "20";
 const numero3 = 30;
 
 
-// Revisar si 2 valores son iguales o diferentes
-console.log(numero1 == numero3);
+// Revisar si 2 numeros son iguales...
+
+// "==" Es un comparador que no es muy estricto.
 console.log(numero1 == numero2);
+console.log(numero1 == numero3);
+
+// "===" Este si es muy estricto.
+console.log(numero1 === parseInt(numero2));
+
+console.log(typeof numero1);
+console.log(typeof numero2);
 
 
-// Typeof
-console.log( typeof numero1 );
-console.log( typeof numero2 );
+// Diferente
 
-// Operador estricto (revisa valor y tipo de dato)
-console.log(numero1 === numero2 );
+const password1 = "admin";
+const password2 = "admiN";
 
-// Diferente a 
-
-console.log(numero1 != numero3);
+console.log(password1 != password2);
 console.log(numero1 != numero2);
-console.log(numero1 !== numero2);
