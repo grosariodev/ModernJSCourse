69f395fb4263d6fd0d74aa01b0d7fac3f66b8187
grosariodev <165927811+grosariodev@users.noreply.github.com>
2025-04-19 06:39:47 -0400

    Finished video for querySelectorAll

---
+++ JSmoderno/Curso JS Moderno/13-DOM/js/05-scripts.js
@@ -0,0 +1,8 @@
+// A diferencia de querySelector, querySelectorAll selecciona todos los elementos llamados por el nombre que tengan, y los almacena en nodos.
+
+const card = document.querySelectorAll('.card');
+console.log(card)
+
+
+const formulario = document.querySelectorAll('#formulario');
+console.log(formulario);
