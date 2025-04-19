d4c3fdf31b89c5a445ed037d89fe2cac6a60ad3e
grosariodev <165927811+grosariodev@users.noreply.github.com>
2025-04-19 08:05:42 -0400

    I learned to modify texts and images with the DOM

---
+++ JSmoderno/Curso JS Moderno/13-DOM/js/06-scripts.js
@@ -0,0 +1,12 @@
+// Modificar textos o imagenes con JS
+
+// const cardR = document.querySelector('.card:nth-child(2) .titulo').textContent = 'Hoy se come pollo';
+// console.log(cardR);
+
+let digamosqueSi = false;
+
+if(digamosqueSi === true){
+    document.querySelector('.card:nth-child(2) .titulo').textContent;
+}else{
+    document.querySelector('.card:nth-child(2) .titulo').textContent = 'Hoy se come pollo';
+}
